5da2132d5caa3cc24a32dfcde051ed9da4d43556
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2025-05-24 16:47:32 -0300

    Sensor API

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -0,0 +1,117 @@
+package com.algaworks.algasensors.device.management.api.controller;
+
+import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.SensorOutput;
+import com.algaworks.algasensors.device.management.common.IdGenerator;
+import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/sensors")
+@RequiredArgsConstructor
+public class SensorController {
+
+    private final SensorRepository sensorRepository;
+
+    @GetMapping
+    public Page<SensorOutput> search (@PageableDefault Pageable pageable) {
+        Page<Sensor> sensors = sensorRepository.findAll(pageable);
+        return sensors.map(this::convertToModel);
+    }
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SensorOutput create(@RequestBody SensorInput input) {
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
+                .name(input.getName())
+                .ip(input.getIp())
+                .location(input.getLocation())
+                .protocol(input.getProtocol())
+                .model(input.getModel())
+                .enabled(false)
+                .build();
+        sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+    @PutMapping("{sensorId}")
+    public SensorOutput output(@RequestBody SensorInput input, @PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensor.setName(input.getName());
+        sensor.setIp(input.getIp());
+        sensor.setLocation(input.getLocation());
+        sensor.setProtocol(input.getProtocol());
+        sensor.setModel(input.getModel());
+
+        sensor = sensorRepository.saveAndFlush(sensor);
+
+        return convertToModel(sensor);
+    }
+
+    @PutMapping("{sensorId}/enable")
+    public ResponseEntity<Void> enable(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensor.setEnabled(true);
+
+        sensorRepository.saveAndFlush(sensor);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{sensorId}/enable")
+    public ResponseEntity<Void> disabled(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensor.setEnabled(false);
+
+        sensorRepository.saveAndFlush(sensor);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{sensorId}")
+    public ResponseEntity<Void> delete(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensorRepository.delete(sensor);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
+        return SensorOutput.builder()
+                .id(sensor.getId().getValue())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
+                .enabled(sensor.getEnabled())
+                .build();
+    }
+
+}
